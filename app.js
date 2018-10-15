23513458c98148ace8833f453098c6fb3dc8c9f9
sidi mohamed <slaghlal@yahoo.fr>
2018-10-15 20:02:55 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ Vue.use(VueRouter)
 const defaultPath = "C:/Users/slagh/Documents/GitHub/ProjetWeb/public"
 const routes = [
   { path: defaultPath + '/index.html', name: 'Login', component: Login},
-  { path: defaultPath + '/search.html', name: 'Search', component: Search}
+  { path: defaultPath + '/search.html', name: 'Search', component: Search},
+  { path: defaultPath + '/registration.html', name: 'Register', component: Register}
 ]
 
 const router = new VueRouter({
@@ -19,14 +20,15 @@ var app = new Vue({
     },
     mounted() {
         console.log(this.authenticated);
-        if(!localStorage.getItem('login') && !window.location.href.includes("public/index.html")) {
+        if(!localStorage.getItem('login') && !window.location.href.includes("public/index.html")
+                  && !window.location.href.includes("public/registration.html")) {
             this.$router.replace({name: 'Login'});
         }
         else if(window.location.href.includes("public/index.html") && localStorage.getItem('login')){
             this.$router.replace({name: 'Search'});
         }
     },
-    components: { Login, Search },
+    components: { Login, Search, Register },
     methods: {
         logout() {
             localStorage.removeItem("login");
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -9,10 +9,14 @@ const Login = Vue.component('login',
     },
     methods: {
         _login(){
-            if(this.login != "" && this.password != "") {
-                if(this.login == "admin" && this.password == "admin") {
-                    localStorage.setItem('login', "admin");
-                    this.$router.replace({name: "Search"})
+
+            var u = JSON.parse(localStorage.getItem(this.login));
+
+            console.log(u.password + ' ' + u.login);
+            if(this.login != "" && this.password != "" && u != null) {
+                if(this.password == u.password) {
+                    localStorage.setItem('login', this.login);
+                    this.$router.replace({name: "Search"});
                 } else {
                     console.log("The username and / or password is incorrect");
                 }
